aa030666b1893158fd9c21b80b43054a6fe5bb5c
Roberts Lusis <roberts.lusis@gmail.com>
2023-11-13 16:05:47 +0200

    commit

--- 2.uzdevums.py
+++ 2.uzdevums.py
@@ -1,7 +1,10 @@
 #Izveidojiet Python programmu, kas izmanto while ciklu, lai atrastu pirmo skaitli, kura kvadrāts ir lielāks par 1000.
-skaitlis=int
-def meklet(mekle):
-    while skaitlis*skaitlis<1000:
-        
+skaitlis=1
+while skaitlis>1000:
+    skaitlis*skaitlis
+
+print("Pirmais skaitlis", skaitlis)
+
+
 
 
