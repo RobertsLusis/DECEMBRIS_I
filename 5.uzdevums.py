5012c5411be57973d251880b38d1837162138a22
Roberts Lusis <roberts.lusis@gmail.com>
2023-11-13 15:59:59 +0200

    commit

--- 5.uzdevums.py
+++ 5.uzdevums.py
@@ -6,9 +6,9 @@ import datetime
 from time import time 
 
 def noteikt_laiku(laiks):
-    if datetime.time<=00 and datetime.time>12:
+    if datetime.time>=00 and datetime.time<12:
         print("labrīt")
-    elif datetime.time<=12 and datetime.time>17:
+    elif datetime.time>=12 and datetime.time<17:
         print("labdien")
-    elif datetime.time<=17 and datetime.time>00:
+    elif datetime.time>=17 and datetime.time<00:
         print("labvakar")
